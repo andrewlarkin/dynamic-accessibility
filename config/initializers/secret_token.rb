93229246d5021fd106172fda48a1c2f95fbf74e5
Andrew Larkin <andrewlarkin2@gmail.com>
2013-07-06 14:53:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DynamicAccessibility::Application.config.secret_key_base = '8b3c55c9e4d11c7005195790a688b9bcf835187d0fed4cfee2a920984f329c65f0143848b383c8fe26f5f49490581052f110857af0f225d1ce10e96c119862ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DynamicAccessibility::Application.config.session_store :cookie_store, key: '_dynamic-accessibility_session'
