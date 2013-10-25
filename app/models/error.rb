70df07c067d202b0b15187da91341f31db9f7302
Andrew Larkin <andrewlarkin2@gmail.com>
2013-10-24 22:56:23 -0400

    Removing unused files

--- app/models/error.rb
+++
@@ -1,6 +0,0 @@
-class Error < ActiveRecord::Base
-  belongs_to :error_rate
-
-  validates :selector, presence: true
-  validates :selector, uniqueness: true
-end
--- app/models/error_rate.rb
+++
@@ -1,6 +0,0 @@
-class ErrorRate < ActiveRecord::Base
-  belongs_to :completed_task
-
-  validates :completed_task_id, :error_id, :error_rate, presence: true
-  validates :error_rate, numericality: { only_integers: true }
-end
